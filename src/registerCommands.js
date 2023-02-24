13c4d6607ecba8bf601e01570ac74d45f538f8c9
Hopp Murithi <hoppmurithi@gmail.com>
2023-02-24 13:42:55 +0300

    feat: implement command registry

---
+++ src/registerCommands.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const { REST, Routes } = require("discord.js");
+
+const commands = [
+  {
+    name: 'hey',
+    description: 'replies with hey!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+  (async () => {
+    try {
+      console.log("server is registering commands");
+
+      await rest.put(
+        Routes.applicationGuildCommands(
+          process.env.CLIENT_ID,
+          process.env.GUILD_ID
+        ),
+        {body: commands}
+      );
+
+      console.log("commands were succefully registered");
+    } catch (error) {
+      console.log(`There was an error: ${error}`);
+    }
+  }
+)();
